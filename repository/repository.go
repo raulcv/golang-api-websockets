6f9d984ec4b4214ed8df0274c92d72e2522ddfa9
Raul <iraulcv@gmail.com>
2023-02-19 20:37:31 -0500

    Posts repositories implementation

--- repository/repository.go
+++ repository/repository.go
@@ -10,6 +10,13 @@ type Repository interface {
 	AddUser(ctx context.Context, user *models.User) error
 	GetUserById(ctx context.Context, id string) (*models.User, error)
 	GetUserByEmail(ctx context.Context, email string) (*models.User, error)
+	AddPost(ctx context.Context, user *models.Post) error
+	GetPostById(ctx context.Context, id string) (*models.Post, error)
+	UpdatePost(ctx context.Context, post *models.Post) error
+	DeletePost(ctx context.Context, id string, userId string) error
+	ActivatePost(ctx context.Context, id string) error
+	ListPost(ctx context.Context, page uint64) ([]*models.Post, error)
+
 	Close() error
 }
 
@@ -30,6 +37,24 @@ func GetUserByEmail(ctx context.Context, email string) (*models.User, error) {
 	return implementation.GetUserByEmail(ctx, email)
 }
 
+func AddPost(ctx context.Context, post *models.Post) error {
+	return implementation.AddPost(ctx, post)
+}
+func GetPostById(ctx context.Context, id string) (*models.Post, error) {
+	return implementation.GetPostById(ctx, id)
+}
+func UpdatePost(ctx context.Context, post *models.Post) error {
+	return implementation.UpdatePost(ctx, post)
+}
+func DeletePost(ctx context.Context, id string, userId string) error {
+	return implementation.DeletePost(ctx, id, userId)
+}
+func ActivatePost(ctx context.Context, id string) error {
+	return implementation.ActivatePost(ctx, id)
+}
+func ListPost(ctx context.Context, page uint64) ([]*models.Post, error) {
+	return implementation.ListPost(ctx, page)
+}
 func Close() error {
 	return implementation.Close()
 }
